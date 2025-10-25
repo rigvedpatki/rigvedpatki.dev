23925c4f8b107c3916a98f0195371bd9b814eff3
Rigved Patki <Rigved.Patki@veritas.com>
2025-10-25 15:31:46 +0530

    chore(cleanup): remove unused project files and configurations

--- src/app/components/side-nav.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-side-nav',
-  imports: [],
-  template: ` <p>side-nav works!</p> `,
-  styles: ``,
-})
-export class SideNav {}
